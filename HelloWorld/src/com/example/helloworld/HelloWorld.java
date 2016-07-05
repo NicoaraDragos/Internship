d5c14be6e39dfecfa4d9b8179f5c5d5015dad116
NicoaraDragos <dragos.nicoara@evozon.com>
2016-07-05 16:27:51 +0300

    HelloWorld Commit

---
+++ HelloWorld/src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.example.helloworld;
+
+/**
+ * Created by dragosnicoara on 7/4/2016.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("somewhere only I know");
+    }
+}
